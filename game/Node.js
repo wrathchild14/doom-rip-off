946f226427de1ef4396d44af2dfd36929d4b093e
wrathchild14 <jovanprodanov123@gmail.com>
2020-12-23 22:05:37 +0100

    progress on fps camera

--- game/Node.js
+++ game/Node.js
@@ -1,11 +1,54 @@
+const vec3 = glMatrix.vec3;
 const mat4 = glMatrix.mat4;
+const quat = glMatrix.quat;
 
 export default class Node {
 
-    constructor() {
-        this.transform = mat4.create();
-        this.children = [];
+    constructor(options = {}) {
+        this.translation = options.translation
+            ? vec3.clone(options.translation)
+            : vec3.fromValues(0, 0, 0);
+        this.rotation = options.rotation
+            ? quat.clone(options.rotation)
+            : quat.fromValues(0, 0, 0, 1);
+        this.scale = options.scale
+            ? vec3.clone(options.scale)
+            : vec3.fromValues(1, 1, 1);
+        this.matrix = options.matrix
+            ? mat4.clone(options.matrix)
+            : mat4.create();
+
+        if (options.matrix) {
+            this.updateTransform();
+        } else if (options.translation || options.rotation || options.scale) {
+            this.updateMatrix();
+        }
+
+        this.camera = options.camera || null;
+        this.mesh = options.mesh || null;
+
+        this.children = [...(options.children || [])];
+        for (const child of this.children) {
+            child.parent = this;
+        }
         this.parent = null;
+
+        this.transform = mat4.create();
+    }
+
+    
+    updateTransform() {
+        mat4.getRotation(this.rotation, this.matrix);
+        mat4.getTranslation(this.translation, this.matrix);
+        mat4.getScaling(this.scale, this.matrix);
+    }
+
+    updateMatrix() {
+        mat4.fromRotationTranslationScale(
+            this.matrix,
+            this.rotation,
+            this.translation,
+            this.scale);
     }
 
     getGlobalTransform() {
@@ -30,12 +73,10 @@ export default class Node {
         }
     }
 
-    traverse(before, after) {
-        before(this);
-        for (let child of this.children) {
-            child.traverse(before, after);
-        }
-        after(this);
+    clone() {
+        return new Node({
+            ...this,
+            children: this.children.map(child => child.clone()),
+        });
     }
-
 }
--- game/game.js
+++ game/game.js
@@ -1,274 +1,97 @@
 import Application from '../../common/Application.js';
 import * as WebGL from './WebGL.js';
+import GLTFLoader from './GLTFLoader.js';
+import Renderer from './Renderer.js';
 
+import PerspectiveCamera from './PerspectiveCamera.js';
 import Node from './Node.js';
+import MyCamera from './MyCamera.js';
+import Physics from './Physics.js';
+import OrthographicCamera from './OrthographicCamera.js';
 
-import shaders from './shaders.js';
-import * as FloorModel from './floor.js';
 
 const mat4 = glMatrix.mat4;
 const vec3 = glMatrix.vec3;
 
 class App extends Application {
 
-    initGL() {
-        const gl = this.gl;
+	async start() {
+		this.loader = new GLTFLoader();
+		// await this.loader.load('../../common/models/monkey/monkey.gltf');
+		// await this.loader.load('../../common/models/test/test.gltf');
+		// await this.loader.load('../../common/models/pyramid/pyramid.gltf');
+		await this.loader.load('../../common/models/myLevel/myLevel.gltf')
 
-        gl.clearColor(0.85, 0.98, 1, 1);
-        gl.enable(gl.DEPTH_TEST);
-        gl.enable(gl.CULL_FACE);
+		this.scene = await this.loader.loadScene(this.loader.defaultScene);
+		this.camera = await this.loader.loadNode('Camera');
 
-        this.programs = WebGL.buildPrograms(gl, shaders);
-    }
+		this.camera = new MyCamera();
+		this.camera.updateTransform();
+		this.camera.updateMatrix();
 
-    initHandlers() {
-        this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
-        this.mousemoveHandler = this.mousemoveHandler.bind(this);
-        this.keydownHandler = this.keydownHandler.bind(this);
-        this.keyupHandler = this.keyupHandler.bind(this);
-        this.keys = {};
+		this.camera.translation = vec3.fromValues(1, 1, 5);
+		this.camera.updateMatrix();
+		this.camera.camera = new PerspectiveCamera();
+		this.scene.addNode(this.camera);
 
-        document.addEventListener('pointerlockchange', this.pointerlockchangeHandler);
-        document.addEventListener('keydown', this.keydownHandler);
-        document.addEventListener('keyup', this.keyupHandler);
-    }
+		// physics can pause
+		// this.physics = new Physics(this.scene);
 
-    start() {
-        const gl = this.gl;
+		this.renderer = new Renderer(this.gl);
+		this.renderer.prepareScene(this.scene);
+		this.resize();
 
-        this.initGL();
-        this.initHandlers();
+		this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
+		document.addEventListener('pointerlockchange', this.pointerlockchangeHandler);
+	}
 
-        const floorModel = this.createModel(FloorModel);
-        const defaultTexture = WebGL.createTexture(gl, {
-            data   : new Uint8Array([255, 255, 255, 255]),
-            width  : 1,
-            height : 1,
-        });
+	enableCamera() {
+		this.canvas.requestPointerLock();
+	}
 
-        this.time = Date.now();
-        this.startTime = this.time;
+	pointerlockchangeHandler() {
+		if (document.pointerLockElement === this.canvas) {
+			this.camera.enable();
+		} else {
+			this.camera.disable();
+		}
+	}
 
-        this.root = new Node();
+	update() {
+		this.time = Date.now();
+		const dt = (this.time - this.startTime) * 0.001;
+		this.startTime = this.time;
 
-        this.camera = new Node();
-        this.root.addChild(this.camera);
-        Object.assign(this.camera, {
-            projection       : mat4.create(),
-            rotation         : vec3.set(vec3.create(), 0, 0, 0),
-            translation      : vec3.set(vec3.create(), 0, 1, 0),
-            velocity         : vec3.set(vec3.create(), 0, 0, 0),
-            mouseSensitivity : 0.002,
-            maxSpeed         : 3,
-            friction         : 0.2,
-            acceleration     : 20
-        });
+		if (this.camera) {
+			this.camera.update(dt);
+		}
 
-        this.floor = new Node();
-        this.floor.model = floorModel;
-        this.floor.texture = defaultTexture;
-        this.root.addChild(this.floor);
-        mat4.fromScaling(this.floor.transform, [10, 1, 10]);
+		if (this.physics) {
+			this.physics.update(dt);
+		}
+	}
 
-        this.loadTexture('../../common/images/grass.png', {
-            mip: true,
-            min: gl.NEAREST_MIPMAP_NEAREST,
-            mag: gl.NEAREST,
-        }, (texture) => {
-            this.floor.texture = texture;
-        });
-    }
+	render() {
+		if (this.renderer) {
+			this.renderer.render(this.scene, this.camera);
+		}
+	}
 
-    update() {
-        this.time = Date.now();
-        const dt = (this.time - this.startTime) * 0.001;
-        this.startTime = this.time;
+	resize() {
+		const w = this.canvas.clientWidth;
+		const h = this.canvas.clientHeight;
+		const aspectRatio = w / h;
 
-        const c = this.camera;
-
-        const forward = vec3.set(vec3.create(),
-            -Math.sin(c.rotation[1]), 0, -Math.cos(c.rotation[1]));
-        const right = vec3.set(vec3.create(),
-            Math.cos(c.rotation[1]), 0, -Math.sin(c.rotation[1]));
-
-        // 1: add movement acceleration
-        let acc = vec3.create();
-        if (this.keys['KeyW']) {
-            vec3.add(acc, acc, forward);
-        }
-        if (this.keys['KeyS']) {
-            vec3.sub(acc, acc, forward);
-        }
-        if (this.keys['KeyD']) {
-            vec3.add(acc, acc, right);
-        }
-        if (this.keys['KeyA']) {
-            vec3.sub(acc, acc, right);
-        }
-
-        // 2: update velocity
-        vec3.scaleAndAdd(c.velocity, c.velocity, acc, dt * c.acceleration);
-
-        // 3: if no movement, apply friction
-        if (!this.keys['KeyW'] &&
-            !this.keys['KeyS'] &&
-            !this.keys['KeyD'] &&
-            !this.keys['KeyA'])
-        {
-            vec3.scale(c.velocity, c.velocity, 1 - c.friction);
-        }
-
-        // 4: limit speed
-        const len = vec3.len(c.velocity);
-        if (len > c.maxSpeed) {
-            vec3.scale(c.velocity, c.velocity, c.maxSpeed / len);
-        }
-
-        // 5: update translation
-        vec3.scaleAndAdd(c.translation, c.translation, c.velocity, dt);
-
-        // 6: update the final transform
-        const t = c.transform;
-        mat4.identity(t);
-        mat4.translate(t, t, c.translation);
-        mat4.rotateY(t, t, c.rotation[1]);
-        mat4.rotateX(t, t, c.rotation[0]);
-    }
-
-    enableMouseLook() {
-        this.canvas.requestPointerLock();
-    }
-
-    pointerlockchangeHandler() {
-        if (document.pointerLockElement === this.canvas) {
-            this.canvas.addEventListener('mousemove', this.mousemoveHandler);
-        } else {
-            this.canvas.removeEventListener('mousemove', this.mousemoveHandler);
-        }
-    }
-
-    mousemoveHandler(e) {
-        const dx = e.movementX;
-        const dy = e.movementY;
-        const c = this.camera;
-        c.rotation[0] -= dy * c.mouseSensitivity;
-        c.rotation[1] -= dx * c.mouseSensitivity;
-
-        const pi = Math.PI;
-        const twopi = pi * 2;
-        const halfpi = pi / 2;
-
-        // Limit pitch
-        if (c.rotation[0] > halfpi) {
-            c.rotation[0] = halfpi;
-        }
-        if (c.rotation[0] < -halfpi) {
-            c.rotation[0] = -halfpi;
-        }
-
-        // Constrain yaw to the range [0, pi * 2]
-        c.rotation[1] = ((c.rotation[1] % twopi) + twopi) % twopi;
-
-    }
-
-    keydownHandler(e) {
-        this.keys[e.code] = true;
-    }
-
-    keyupHandler(e) {
-        this.keys[e.code] = false;
-    }
-
-    render() {
-        const gl = this.gl;
-
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        const program = this.programs.simple;
-        gl.useProgram(program.program);
-
-        gl.activeTexture(gl.TEXTURE0);
-        gl.uniform1i(program.uniforms.uTexture, 0);
-
-        let mvpMatrix = mat4.create();
-        let mvpStack = [];
-        const mvpLocation = program.uniforms.uModelViewProjection;
-        const viewMatrix = this.camera.getGlobalTransform();
-        mat4.invert(viewMatrix, viewMatrix);
-        mat4.mul(mvpMatrix, this.camera.projection, viewMatrix);
-
-        this.root.traverse(
-            (node) => {
-                mvpStack.push(mat4.clone(mvpMatrix));
-                mat4.mul(mvpMatrix, mvpMatrix, node.transform);
-                if (node.model) {
-                    gl.bindVertexArray(node.model.vao);
-                    gl.uniformMatrix4fv(mvpLocation, false, mvpMatrix);
-                    gl.bindTexture(gl.TEXTURE_2D, node.texture);
-                    gl.drawElements(gl.TRIANGLES, node.model.indices, gl.UNSIGNED_SHORT, 0);
-                }
-            },
-            (node) => {
-                mvpMatrix = mvpStack.pop();
-            }
-        );
-    }
-
-    resize() {
-        const w = this.canvas.clientWidth;
-        const h = this.canvas.clientHeight;
-        const aspect = w / h;
-        const fovy = Math.PI / 2;
-        const near = 0.1;
-        const far = 100;
-
-        mat4.perspective(this.camera.projection, fovy, aspect, near, far);
-    }
-
-    createModel(model) {
-        const gl = this.gl;
-
-        const vao = gl.createVertexArray();
-        gl.bindVertexArray(vao);
-
-        const vertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, model.vertices, gl.STATIC_DRAW);
-
-        const indices = model.indices.length;
-        const indexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, model.indices, gl.STATIC_DRAW);
-
-        gl.enableVertexAttribArray(0);
-        gl.enableVertexAttribArray(1);
-
-        gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 24, 0);
-        gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 24, 16);
-
-        return { vao, indices };
-    }
-
-    loadTexture(url, options, handler) {
-        const gl = this.gl;
-
-        let image = new Image();
-        image.addEventListener('load', () => {
-            const opts = Object.assign({ image }, options);
-            handler(WebGL.createTexture(gl, opts));
-        });
-        image.src = url;
-    }
-
+		if (this.camera) {
+			this.camera.camera.aspect = aspectRatio;
+			this.camera.camera.updateMatrix();
+		}
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.querySelector('canvas');
-    const app = new App(canvas);
-    const gui = new dat.GUI();
-    gui.add(app.camera, 'mouseSensitivity', 0.0001, 0.01);
-    gui.add(app.camera, 'maxSpeed', 0, 10);
-    gui.add(app.camera, 'friction', 0.05, 0.75);
-    gui.add(app.camera, 'acceleration', 1, 100);
-    gui.add(app, 'enableMouseLook');
+	const canvas = document.querySelector('canvas');
+	const app = new App(canvas);
+	const gui = new dat.GUI();
+	gui.add(app, 'enableCamera');
 });
